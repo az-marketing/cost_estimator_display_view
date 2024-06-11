84bb9ce7a04bac420ccf9da777753887f1bf395f
Raindog <128643925+ewlyman@users.noreply.github.com>
2024-06-11 15:03:15 -0700

    Add files via upload

---
+++ js/cost_estimator_page_view.js
@@ -0,0 +1,23 @@
+(function ($) {
+    $(window).on('load', function () {
+
+     $("#edit-nid-entityreference-filter").val("All");
+  
+  $("#edit-submit-cost-estimator-block").attr("disabled", "true");
+   
+    $("#views-exposed-form-cost-estimator-block-page-1 select").change(function() {
+    var levalue = $("#edit-field-career-level-target-id").val();
+    var devalue = $("#edit-nid-entityreference-filter").val();
+
+     
+     if (!(levalue == "All") && !(devalue == "All")) {
+     $("#edit-submit-cost-estimator-block").removeAttr("disabled");
+          } else {
+     $("#edit-submit-cost-estimator-block").attr("disabled", "true");
+          }
+
+}); 
+   
+     });
+        
+}) (jQuery);
